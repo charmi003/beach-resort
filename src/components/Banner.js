3aab743cc24bb199a87a72d06acaa72307b73d75
charmi003 <56063551+charmi003@users.noreply.github.com>
2021-09-23 23:20:31 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import H1 from './H1'
 
 const Banner=({title,subtitle,children})=> {
     return (
-        <div className='banner text-center py-8 px-20 border'>
+        <div className='banner text-center py-8 px-10 md:px-20 border'>
             <H1>{title}</H1>
             <div className='line-lg mx-auto my-6'></div>
             <p className='subtitle font-semibold capitalize'>{subtitle}</p>
